dc03f831ae117f8dc5fc5fcb585742828b3a5bb4
Nik Gianitsos <gianitsos04@icloud.com>
2017-11-04 12:39:17 -0500

    Max subarray slow

--- misc/max_subarray/MaxSubarray.java
+++ misc/max_subarray/MaxSubarray.java
@@ -4,9 +4,9 @@ You are given an array of ints.
 Find the max sum of consecutive elements.
 
 input format:
-The first number will be the number of test cases
-The second number N will be the length of the array
-The third line will be N numbers that are in the array
+The first line will be the number of test cases
+In each test case, the first number N will be the length of the array
+The second line in each test case will be the N numbers that are in the array
 
 in:
 2
@@ -21,9 +21,49 @@ out:
 */
 
 import java.util.*;
+import java.io.*;
 
 class MaxSubarray {
 	public static void main(String[] args) {
-		
+		run(System.in, System.out);
+
+	}
+
+	private static void run(InputStream in, PrintStream out) {
+		Scanner scan = new Scanner(in);
+		int testCases = scan.nextInt();
+		scan.nextLine(); 
+		for (int currentCase = 0; currentCase < testCases; currentCase++) {
+			int arrayLength = scan.nextInt();
+			scan.nextLine();
+			int[] array = new int[arrayLength];
+			for (int i = 0; i < array.length; i++) {
+				array[i] = scan.nextInt();
+			}
+			scan.nextLine();
+
+			int bigSum = Integer.MIN_VALUE;
+			for (int i = 0; i < array.length; i++) {
+				int sum = 0;
+				for (int j = i; j < array.length; j++) {
+					sum += array[j];
+					if (sum > bigSum) {
+						bigSum = sum;
+					}
+				}
+			}
+			out.println(bigSum);
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
