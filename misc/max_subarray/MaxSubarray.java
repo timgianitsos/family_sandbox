1796581ecbd8da86efdddb4a629d5590a979270d
Nik Gianitsos <gianitsos04@icloud.com>
2017-11-05 13:48:45 -0600

    Kadane's algorithm

--- misc/max_subarray/MaxSubarray.java
+++ misc/max_subarray/MaxSubarray.java
@@ -18,6 +18,36 @@ in:
 out:
 7
 8
+
+in:
+8
+3
+8 -7 1000
+3
+8 -8 1000
+3
+8 -9 1000
+5
+1 1 1 -4 1000
+5
+3 -1 5 -20 2
+5
+-3 -5 -1 -2 -14
+5
+3 5 1 2 14
+1
+-13
+
+out:
+1001
+1000
+1000
+1000
+7
+-1
+25
+-13
+
 */
 
 import java.util.*;
@@ -25,11 +55,10 @@ import java.io.*;
 
 class MaxSubarray {
 	public static void main(String[] args) {
-		run(System.in, System.out);
-
+		runFast(System.in, System.out);
 	}
 
-	private static void run(InputStream in, PrintStream out) {
+	private static void runSlow(InputStream in, PrintStream out) {
 		Scanner scan = new Scanner(in);
 		int testCases = scan.nextInt();
 		scan.nextLine(); 
@@ -55,6 +84,36 @@ class MaxSubarray {
 			out.println(bigSum);
 		}
 	}
+
+	//Kadane's algorithm
+	private static void runFast(InputStream in, PrintStream out) {
+		Scanner scan = new Scanner(in);
+		int testCases = scan.nextInt();
+		scan.nextLine();
+		for (int currentCase = 0; currentCase < testCases; currentCase++) {
+			int arrayLength = scan.nextInt();
+			scan.nextLine();
+			int[] array = new int[arrayLength];
+			for (int i = 0; i < array.length; i++) {
+				array[i] = scan.nextInt();
+			}
+			scan.nextLine();
+			int sum = 0;
+			int bigSum = Integer.MIN_VALUE;
+			for (int i = 0; i < array.length; i++) {
+				if (sum < 0) {
+					sum = 0;
+				}
+				sum += array[i];
+				if (bigSum < sum) {
+					bigSum = sum;
+				}
+			}
+			out.println(bigSum);
+		}
+
+	}
+
 }
 
 
--- misc/max_subarray/TestSubarray.java
+++ misc/max_subarray/TestSubarray.java
@@ -17,7 +17,13 @@ class TestSubarray {
 		{
 			String input = "2\n6\n-1 5 2 -2 1 -1\n8\n-1 5 -2 3 0 -1 2 1\n";
 			String expectedOutput = "7\n8\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("run", InputStream.class, PrintStream.class);
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "8\n3\n8 -7 1000\n3\n8 -8 1000\n3\n8 -9 1000\n5\n1 1 1 -4 1000\n5\n3 -1 5 -20 2\n5\n-3 -5 -1 -2 -14\n5\n3 5 1 2 14\n1\n-13\n";
+			String expectedOutput = "1001\n1000\n1000\n1000\n7\n-1\n25\n-13\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
 			TestHarness.test(input, expectedOutput, m);
 		}
 		{
@@ -32,7 +38,7 @@ class TestSubarray {
 
 			String input = sb.toString();
 			String expectedOutput = "0\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("run", InputStream.class, PrintStream.class);
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
 			TestHarness.test(input, expectedOutput, m);
 		}
 		{
@@ -47,7 +53,49 @@ class TestSubarray {
 
 			String input = sb.toString();
 			String expectedOutput = "0\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("run", InputStream.class, PrintStream.class);
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "2\n6\n-1 5 2 -2 1 -1\n8\n-1 5 -2 3 0 -1 2 1\n";
+			String expectedOutput = "7\n8\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "8\n3\n8 -7 1000\n3\n8 -8 1000\n3\n8 -9 1000\n5\n1 1 1 -4 1000\n5\n3 -1 5 -20 2\n5\n-3 -5 -1 -2 -14\n5\n3 5 1 2 14\n1\n-13\n";
+			String expectedOutput = "1001\n1000\n1000\n1000\n7\n-1\n25\n-13\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			int numElements = 40000;
+			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
+			sb.append("1\n" + numElements + "\n");
+			for (int i = 0; i < numElements; i++) {
+				sb.append('0');
+				sb.append(' ');
+			}
+			sb.append('\n');
+
+			String input = sb.toString();
+			String expectedOutput = "0\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			int numElements = 80000;
+			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
+			sb.append("1\n" + numElements + "\n");
+			for (int i = 0; i < numElements; i++) {
+				sb.append('0');
+				sb.append(' ');
+			}
+			sb.append('\n');
+
+			String input = sb.toString();
+			String expectedOutput = "0\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
 			TestHarness.test(input, expectedOutput, m);
 		}
 
