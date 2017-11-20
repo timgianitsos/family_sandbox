03d78f8694eb6ddfbf642fddf3b4ce15954492c2
Nik Gianitsos <gianitsos04@icloud.com>
2017-11-19 18:49:55 -0600

    Finished stocks

--- misc/max_subarray/Stocks.java
+++ misc/max_subarray/Stocks.java
@@ -5,17 +5,19 @@ Find the best time to buy a stock, and the best time to sell
 Each stock price is at minute intervals i.e. the seond price is 
 one minute after the first. There will be at east two stock prices.
 
+Print the earliest best buy time if there are multiple best buy times
+
 input:
 The first line will be n, the number of prices in a day
 The second line will be all the prices in a day
 
 in:
-5
-5 15 3 9 14 12
+6
+5 15 6 3 9 14 12
 
 out:
-buy @ time 2
-sell @ time 4
+buy @ time 3
+sell @ time 5
 
 */
 import java.util.*;
@@ -24,8 +26,8 @@ import java.io.*;
 class Stocks {
 	public static void main(String[] args) {
 		run(System.in, System.out);
-
 	}
+
 	private static void run(InputStream in, PrintStream out) {
 		Scanner scan = new Scanner(in);
 		int n = scan.nextInt();
@@ -36,23 +38,42 @@ class Stocks {
 		}
 		scan.nextLine();
 
-		int currentDifference = 0;
-		int bestSell = Integer.MIN_VALUE;
-		int bestBuy = Integer.MAX_VALUE;
-		int buy = -1;
-		int sell = -1;
-		for (int i = 0; i < (stocks.length - 1); i++) {
-			currentDifference = stocks[i] - stocks[i + 1];
-			if (currentDifference > bestSell) {
-				bestSell = currentDifference;
-				sell = i;			
+		int bestBuyIndex = 0;
+		int buyIndex = 0;
+		int sellIndex = 0;
+		int mostProfit = Integer.MIN_VALUE;
+		int currentProfit = 0;
+		for (int i = 0; i < stocks.length; i++) {
+			currentProfit = stocks[i] - stocks[buyIndex];
+			if (currentProfit > mostProfit) {
+				mostProfit = currentProfit;
+				sellIndex = i;
+				bestBuyIndex = buyIndex;
 			}
-			if (currentDifference < bestBuy) {
-				bestBuy = currentDifference;
-				buy = i;
+			else if (currentProfit < 0) {
+				buyIndex = i;
 			}
 		}
-		out.println("buy @ time " + buy + "\nsell @ time " + sell);
+		out.println("buy @ time " + bestBuyIndex + "\nsell @ time " + sellIndex);
+
+
+		// int currentDifference = 0;
+		// int bestSell = Integer.MIN_VALUE;
+		// int bestBuy = Integer.MAX_VALUE;
+		// int buy = -1;
+		// int sell = -1;
+		// for (int i = 0; i < (stocks.length - 1); i++) {
+		// 	currentDifference = stocks[i] - stocks[i + 1];
+		// 	if (currentDifference > bestSell) {
+		// 		bestSell = currentDifference;
+		// 		sell = i;			
+		// 	}
+		// 	if (currentDifference < bestBuy) {
+		// 		bestBuy = currentDifference;
+		// 		buy = i;
+		// 	}
+		// }
+		// out.println("buy @ time " + buy + "\nsell @ time " + sell);
 	}
 }
 
--- misc/max_subarray/TestSubarray.java
+++ misc/max_subarray/TestSubarray.java
@@ -12,97 +12,121 @@ class TestSubarray {
 	public static void main(String[] args) throws NoSuchMethodException {
 		System.out.println();
 
-		//Write tests here
-		//input and output are always string objects
-		{
-			String input = "2\n6\n-1 5 2 -2 1 -1\n8\n-1 5 -2 3 0 -1 2 1\n";
-			String expectedOutput = "7\n8\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
-		{
-			String input = "8\n3\n8 -7 1000\n3\n8 -8 1000\n3\n8 -9 1000\n5\n1 1 1 -4 1000\n5\n3 -1 5 -20 2\n5\n-3 -5 -1 -2 -14\n5\n3 5 1 2 14\n1\n-13\n";
-			String expectedOutput = "1001\n1000\n1000\n1000\n7\n-1\n25\n-13\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
-		{
-			int numElements = 40000;
-			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
-			sb.append("1\n" + numElements + "\n");
-			for (int i = 0; i < numElements; i++) {
-				sb.append('0');
-				sb.append(' ');
-			}
-			sb.append('\n');
-
-			String input = sb.toString();
-			String expectedOutput = "0\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
-		{
-			int numElements = 80000;
-			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
-			sb.append("1\n" + numElements + "\n");
-			for (int i = 0; i < numElements; i++) {
-				sb.append('0');
-				sb.append(' ');
-			}
-			sb.append('\n');
-
-			String input = sb.toString();
-			String expectedOutput = "0\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
-		{
-			String input = "2\n6\n-1 5 2 -2 1 -1\n8\n-1 5 -2 3 0 -1 2 1\n";
-			String expectedOutput = "7\n8\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
-		{
-			String input = "8\n3\n8 -7 1000\n3\n8 -8 1000\n3\n8 -9 1000\n5\n1 1 1 -4 1000\n5\n3 -1 5 -20 2\n5\n-3 -5 -1 -2 -14\n5\n3 5 1 2 14\n1\n-13\n";
-			String expectedOutput = "1001\n1000\n1000\n1000\n7\n-1\n25\n-13\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
-		{
-			int numElements = 40000;
-			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
-			sb.append("1\n" + numElements + "\n");
-			for (int i = 0; i < numElements; i++) {
-				sb.append('0');
-				sb.append(' ');
-			}
-			sb.append('\n');
-
-			String input = sb.toString();
-			String expectedOutput = "0\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
-		{
-			int numElements = 80000;
-			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
-			sb.append("1\n" + numElements + "\n");
-			for (int i = 0; i < numElements; i++) {
-				sb.append('0');
-				sb.append(' ');
-			}
-			sb.append('\n');
-
-			String input = sb.toString();
-			String expectedOutput = "0\n";
-			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
-			TestHarness.test(input, expectedOutput, m);
-		}
+		// maxSubArrayTests();
+		stockTests();
 
 		//Results
 		int testsRun = TestHarness.getTestsRun();
 		int failedTests = TestHarness.getFailedTests();
 		System.out.print("\n" + TestHarness.YELLOW + "Ran " + testsRun + " test(s): ");
 		System.out.println((failedTests == 0 ? "Success!": "Failed " + failedTests) + TestHarness.RESET);
+	}
+
+	private static void stockTests() throws NoSuchMethodException {
+		{
+			String input = "6\n5 15 6 3 9 14 12\n";
+			String expectedOutput = "buy @ time 3\nsell @ time 5\n";
+			Method m = Stocks.class.getDeclaredMethod("run", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "4\n4 600 2 20\n";
+			String expectedOutput = "buy @ time 0\nsell @ time 1\n";
+			Method m = Stocks.class.getDeclaredMethod("run", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "9\n4 400000 400001 3 30000 3000001 5 50005505 1\n";
+			String expectedOutput = "buy @ time 3\nsell @ time 7\n";
+			Method m = Stocks.class.getDeclaredMethod("run", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+	}
+
+	private static void maxSubArrayTests() throws NoSuchMethodException {
+		{
+			String input = "2\n6\n-1 5 2 -2 1 -1\n8\n-1 5 -2 3 0 -1 2 1\n";
+			String expectedOutput = "7\n8\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "8\n3\n8 -7 1000\n3\n8 -8 1000\n3\n8 -9 1000\n5\n1 1 1 -4 1000\n5\n3 -1 5 -20 2\n5\n-3 -5 -1 -2 -14\n5\n3 5 1 2 14\n1\n-13\n";
+			String expectedOutput = "1001\n1000\n1000\n1000\n7\n-1\n25\n-13\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			int numElements = 40000;
+			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
+			sb.append("1\n" + numElements + "\n");
+			for (int i = 0; i < numElements; i++) {
+				sb.append('0');
+				sb.append(' ');
+			}
+			sb.append('\n');
+
+			String input = sb.toString();
+			String expectedOutput = "0\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			int numElements = 80000;
+			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
+			sb.append("1\n" + numElements + "\n");
+			for (int i = 0; i < numElements; i++) {
+				sb.append('0');
+				sb.append(' ');
+			}
+			sb.append('\n');
+
+			String input = sb.toString();
+			String expectedOutput = "0\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runSlow", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "2\n6\n-1 5 2 -2 1 -1\n8\n-1 5 -2 3 0 -1 2 1\n";
+			String expectedOutput = "7\n8\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			String input = "8\n3\n8 -7 1000\n3\n8 -8 1000\n3\n8 -9 1000\n5\n1 1 1 -4 1000\n5\n3 -1 5 -20 2\n5\n-3 -5 -1 -2 -14\n5\n3 5 1 2 14\n1\n-13\n";
+			String expectedOutput = "1001\n1000\n1000\n1000\n7\n-1\n25\n-13\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			int numElements = 40000;
+			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
+			sb.append("1\n" + numElements + "\n");
+			for (int i = 0; i < numElements; i++) {
+				sb.append('0');
+				sb.append(' ');
+			}
+			sb.append('\n');
+
+			String input = sb.toString();
+			String expectedOutput = "0\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
+		{
+			int numElements = 80000;
+			StringBuilder sb = new StringBuilder(2 * numElements + 4 + String.valueOf(numElements).length());
+			sb.append("1\n" + numElements + "\n");
+			for (int i = 0; i < numElements; i++) {
+				sb.append('0');
+				sb.append(' ');
+			}
+			sb.append('\n');
+
+			String input = sb.toString();
+			String expectedOutput = "0\n";
+			Method m = MaxSubarray.class.getDeclaredMethod("runFast", InputStream.class, PrintStream.class);
+			TestHarness.test(input, expectedOutput, m);
+		}
 	}
 }
