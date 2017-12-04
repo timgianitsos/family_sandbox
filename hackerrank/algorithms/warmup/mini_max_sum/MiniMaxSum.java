0b687a57247c8b5c9827df576255c815efdf8e80
Nik Gianitsos <gianitsos04@icloud.com>
2017-12-03 20:33:39 -0600

    Minimax sum

--- hackerrank/algorithms/warmup/mini_max_sum/MiniMaxSum.java
+++ hackerrank/algorithms/warmup/mini_max_sum/MiniMaxSum.java
@@ -15,8 +15,43 @@ class MiniMaxSum {
 		}
 		scan.close();
 
+		long maxSum = 0;
+		long miniSum = 0;
+		int bigInt = Integer.MIN_VALUE;
+		int smallInt = Integer.MAX_VALUE;
+		int bigIndex =  -1;
+		int smallIndex = -1;
 		for (int i = 0; i < array.length; i++) {
-			
+			if (array[i] > bigInt) {
+				bigInt = array[i];
+				bigIndex = i;
+			}
+			if (array[i] < smallInt) {
+				smallInt = array[i];
+				smallIndex = i;
+			}
 		}
+		for (int i = 0; i < array.length; i++) {
+			if (i != smallIndex) {
+				maxSum += array[i];
+			}
+			if (i != bigIndex) {
+				miniSum += array[i];
+			}
+		}
+		out.println(miniSum + " " + maxSum);
+
+		/*
+		Tim's solution
+		long sum = 0;
+		int big = Integer.MIN_VALUE;
+		int small = Integer.MAX_VALUE;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+			big = big < array[i] ? array[i]: big;
+			small = small > array[i] ? array[i]: small;
+		}
+		out.println((sum - big) + " " + (sum - small));
+		*/
 	}
 }
