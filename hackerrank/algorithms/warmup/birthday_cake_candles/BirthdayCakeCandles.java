6bcb252aad84a82be5edb9ff594e29579701d4e8
Nik Gianitsos <gianitsos04@icloud.com>
2017-12-05 19:58:12 -0600

    Birthdaycake candle initial

---
+++ hackerrank/algorithms/warmup/birthday_cake_candles/BirthdayCakeCandles.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class BirthdayCakeCandles {
+	public static void main(String[] args) {
+		run(System.in, System.out); 
+	}
+	private static void run(InputStream in, PrintStream out) {
+		Scanner scan = new Scanner(in);
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		int bigCandle = Integer.MIN_VALUE;
+		int counter = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (i > bigCandle) {
+				bigCandle = i;
+				counter++;
+			}
+			
+			else if (i == bigCandle) {
+				counter++;
+			}
+		}
+	}
+}
