2f318c2b9d0cb337d19b2ca4f90080a0a9e518f3
Nik Gianitsos <gianitsos04@icloud.com>
2017-12-09 14:41:03 -0600

    Integer implementation

--- codeforces/lazergun/LazerGun.java
+++ codeforces/lazergun/LazerGun.java
@@ -1,6 +1,9 @@
 import java.util.*;
 import java.io.*;
 
+/*
+http://codeforces.com/problemset/problem/514/B?mobile=true
+*/
 class LazerGun {
 	public static void main(String[] args) {
 		run(System.in, System.out);
@@ -21,5 +24,53 @@ class LazerGun {
 
 		Set<Integer> slopes = new HashSet<>(); 
 
+		for (int i = 0; i < enemyX.length; i++) {
+			if ((myX - enemyX[i]) != 0) {
+				int eSlope = (myY - enemyY[i]) / (myX - enemyX[i]);
+out.println("eSlope: " + eSlope);
+				slopes.add(eSlope);
+			}
+			else {
+				slopes.add(null);
+			}
+		}
+		out.println(slopes.size());
 	}
 }
+
+/*
+in:
+4 0 0
+1 1
+2 2
+2 0
+-1 -1
+
+out:
+2
+
+in:
+2 1 2
+1 1
+1 0
+
+out:
+1
+
+in:
+10 -4 -4
+2 -4
+2 0
+-4 2
+5 -4
+-3 -5
+1 4
+-4 2
+-3 5
+0 -3
+2 4
+
+out:
+8
+*/
+
