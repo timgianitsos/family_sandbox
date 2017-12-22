9ab9d3468d8bb3c150be0714550361ffaa3e26e7
Nik Gianitsos <gianitsos04@icloud.com>
2017-12-22 12:52:12 -0600

    Fixed 0 in hour and 12AM and PM conversion

--- hackerrank/algorithms/warmup/time_conversion/TimeConversion.java
+++ hackerrank/algorithms/warmup/time_conversion/TimeConversion.java
@@ -14,11 +14,20 @@ class TimeConversion {
 		String end2 = time.substring(time.length() - 2);
 		int hour = Integer.parseInt(time.substring(0, 2));
 		int mHour = hour;
-		if (end2.equals("PM")) {
+		if ((end2.equals("PM") && hour != ARMY_TIME_DIFFERENCE) || end2.equals("AM") && hour == ARMY_TIME_DIFFERENCE) {
 			mHour = hour + ARMY_TIME_DIFFERENCE;
+			if (mHour >= 24) {
+				mHour = 0;
+			}
 		}
 		String modified = time.substring(0, time.length() - 2);
 		String armyTime = modified.substring(2);
-		out.println(mHour + armyTime);
+		if (mHour < 10) {
+			out.println("0" + mHour + armyTime);
+		}
+		else {
+			out.println(mHour + armyTime);
+		}
+		
 	}
 }
