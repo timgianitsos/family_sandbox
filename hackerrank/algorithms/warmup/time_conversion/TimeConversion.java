008ee1462c3de36a960b51af1db0a14c56722cf1
Nik Gianitsos <gianitsos04@icloud.com>
2017-12-22 12:32:16 -0600

    conversion to military time

--- hackerrank/algorithms/warmup/time_conversion/TimeConversion.java
+++ hackerrank/algorithms/warmup/time_conversion/TimeConversion.java
@@ -2,15 +2,23 @@ import java.util.*;
 import java.io.*;
 
 class TimeConversion {
+	private static final int ARMY_TIME_DIFFERENCE = 12;
 	public static void main(String[] args) {
 		run(System.in, System.out);
 	}
 	private static void run(InputStream in, PrintStream out) {
 		Scanner scan = new Scanner(in);
 		String time = scan.nextLine();
-		String end2 = time.substring(time.length() - 2, time.length() - 1);
-		if (end2.equals("AM")) {
-			out.println(time.substring(0, time.length() - 1));
+		scan.close();
+
+		String end2 = time.substring(time.length() - 2);
+		int hour = Integer.parseInt(time.substring(0, 2));
+		int mHour = hour;
+		if (end2.equals("PM")) {
+			mHour = hour + ARMY_TIME_DIFFERENCE;
 		}
+		String modified = time.substring(0, time.length() - 2);
+		String armyTime = modified.substring(2);
+		out.println(mHour + armyTime);
 	}
 }
