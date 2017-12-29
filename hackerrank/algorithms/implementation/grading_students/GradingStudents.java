9498ee400630d31b48691e10d72f019764a07524
Nik Gianitsos <gianitsos04@icloud.com>
2017-12-29 17:22:14 -0600

    Multiple of five

--- hackerrank/algorithms/implementation/grading_students/GradingStudents.java
+++ hackerrank/algorithms/implementation/grading_students/GradingStudents.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
 class GradingStudent {
-	public static final FAILING = 40;
+	public static final int FAILING = 40;
 
 	public static void main(String args[]) {
 		run(System.in, System.out);
 	}
 
 	private static void run(InputStream in, PrintStream out) {
-		Scanner scan = new Scanner();
+		Scanner scan = new Scanner(in);
 		int students = scan.nextInt();
 		int[] grades = new int[students];
 		for (int i = 0; i < grades.length; i++) {
@@ -18,8 +18,16 @@ class GradingStudent {
 		scan.close();
 		
 		for (int i = 0; i < grades.length; i++) {
-			if ((grades[i] + 2) < FAILING || (grades[i] + 2) < /*TODO if grade is less than 2 away from next multiple of five*/ ) {
-			out.print(grades[i]);
+			int grade = grades[i];
+			int remander = grade % 5;
+			int difference = 5 - remander;
+			int nextMultiple = grade + difference;
+
+			if ((grade + 2) < FAILING || (grade + 2) < nextMultiple) {
+				out.println(grade);
+			}
+			else {
+				out.println(nextMultiple);
 			}
 		
 		}
