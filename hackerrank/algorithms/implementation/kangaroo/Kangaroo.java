964dc09867584b9f678b0fb9781938678092dfd3
Nik Gianitsos <gianitsos04@icloud.com>
2017-12-30 18:47:39 -0600

    Kangaroo finish

--- hackerrank/algorithms/implementation/kangaroo/Kangaroo.java
+++ hackerrank/algorithms/implementation/kangaroo/Kangaroo.java
@@ -11,17 +11,22 @@ class Kangaroo {
 		int v1 = scan.nextInt();
 		int x2 = scan.nextInt();
 		int v2 = scan.nextInt();
+		int currentX1 = x1;
+		int currentX2 = x2;
 		scan.close();
 
-		if ( v2 >= v1) {
+		if (v2 >= v1) {
 			out.println("NO");
+			return;
 		}
-		else if (v2 < v1 /* <- REMOVE */) { 
-			out.println("NO");
+
+		while (currentX1 < currentX2) {
+			currentX1 += v1;
+			currentX2 += v2;
 		}
-		else {
-			out.println("YES");
-		}
+		out.println(currentX1 == currentX2 ? "YES" : "NO");
+
+		
 
 
 
